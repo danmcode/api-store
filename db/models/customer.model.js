aaf991767cc239b1c27fcd9d6a9e1f796c2660db
Daniel Alexander Muelas Rivera <danmcode@gmail.com>
2025-08-31 21:35:09 -0500

    adding a customer module service

---
+++ db/models/customer.model.js
@@ -0,0 +1,68 @@
+const { Model, DataTypes, Sequelize } = require("sequelize");
+const { USER_TABLE } = require("./user.model");
+
+const CUSTOMER_TABLE = 'customers';
+
+const CustomerSchema = {
+    id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+    },
+    name: {
+        allowNull: false,
+        type: DataTypes.STRING
+    },
+    lastName: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        field: 'last_name'
+    },
+    phone: {
+        allowNull: false,
+        type: DataTypes.STRING
+    },
+    userId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        field: 'user_id',
+        references: {
+            model: USER_TABLE,
+            key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+    },
+    createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'created_at'
+    },
+    updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        field: 'updated_at'
+    }
+
+};
+
+class Customer extends Model {
+    static associate(models) {
+        this.belongsTo(models.User, {
+            foreignKey: 'userId',
+            as: 'user'
+        });
+    }
+
+    static config(sequelize) {
+        return {
+            sequelize,
+            tableName: CUSTOMER_TABLE,
+            modelName: 'Customer',
+            timestamps: false
+        }
+    }
+}
+
+module.exports = { CUSTOMER_TABLE, CustomerSchema, Customer };
--- db/models/index.js
+++ db/models/index.js
@@ -1,7 +1,12 @@
+const { Customer, CustomerSchema } = require("./customer.model");
 const { User, UserSchema } = require("./user.model");
+
 
 function setupModels(sequelize) {
     User.init(UserSchema, User.config(sequelize));
+    Customer.init(CustomerSchema, Customer.config(sequelize));
+
+    Customer.associate(sequelize.models);
 }
 
 module.exports = setupModels;
