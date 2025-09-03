0d82ce17625379a5d80b9bf1176531d8fae71ba2
Daniel Alexander Muelas Rivera <danmcode@gmail.com>
2025-09-02 21:52:47 -0500

    feat: adding orders module

--- db/models/index.js
+++ db/models/index.js
@@ -1,6 +1,7 @@
-const { Category } = require("./category.model");
+const { Category, CategorySchema } = require("./category.model");
 const { Customer, CustomerSchema } = require("./customer.model");
-const { Product } = require("./product.model");
+const { Order, OrderSchema } = require("./order.model");
+const { Product, ProductSchema } = require("./product.model");
 const { User, UserSchema } = require("./user.model");
 
 
@@ -9,10 +10,12 @@ function setupModels(sequelize) {
     Customer.init(CustomerSchema, Customer.config(sequelize));
     Category.init(CategorySchema, Category.config(sequelize));
     Product.init(ProductSchema, Product.config(sequelize));
+    Order.init(OrderSchema, Order.config(sequelize));
 
     Customer.associate(sequelize.models);
     Category.associate(sequelize.models);
     Product.associate(sequelize.models);
+    Order.associate(sequelize.models);
 }
 
 module.exports = setupModels;
